b57dd714cbf30bb5adb7b0dc109156aca15a8302
Kimokot <bkim19302@gmail.com>
2016-08-26 09:45:49 +0300

    PEP 8 formatting

--- quizzactions.py
+++ quizzactions.py
@@ -8,17 +8,20 @@ from time import sleep
 from tqdm import tqdm
 from timer import AlarmException, alarmHandler, nonBlockingRawInput
 from shutil import copy2
-import os, random
+import os
+import random
 import click
 import json
 import easygui
 import urllib
-import time, sys
+import time
+import sys
 import warnings
 
 warnings.filterwarnings("ignore")
 
-firebase = firebase.FirebaseApplication('https://python-ac720.firebaseio.com', None)
+firebase = firebase.FirebaseApplication(
+    'https://python-ac720.firebaseio.com', None)
 
 
 def init_screen():
@@ -35,40 +38,42 @@ def init_screen():
             ["quiz_take", "<quiz name>", "Take a quiz"], ["download_quiz",
                                                           "<quiz name>", "Download quiz from remore repo"],
             ["upload_quizz ", "<quiz_name>", "Upload quiz to remote repo"],
-            ["easy_import","","Simpler file selector to Import File"],
-            ["logout","","Exit QuizzApp"]
-            ]
+            ["easy_import", "", "Simpler file selector to Import File"],
+            ["logout", "", "Exit QuizzApp"]
+        ]
 
         print colored(tabulate(commands, headers=["Command", "Arguments", "Description"], tablefmt="psql"), "green")
         print colored("\n\t\tEnter help <command name> to get help\n", "yellow")
-        delay_print("\tThe Quizz App Allows a user to view quizzes stored both locally and remotely,"\
-         "\n\tupload local quizzes to remote host, download from remote to local and and"\
-         " \n\ttake quizzes and get graded.")
+        delay_print("\tThe Quizz App Allows a user to view quizzes stored both locally and remotely,"
+                    "\n\tupload local quizzes to remote host, download from remote to local and and"
+                    " \n\ttake quizzes and get graded.")
         print("\n")
     except KeyboardInterrupt:
         print('\n')
         sys.exit()
 
+
 def delay_print(s):
     for c in s:
-        sys.stdout.write( '%s' % c )
+        sys.stdout.write('%s' % c)
         sys.stdout.flush()
         time.sleep(0.01)
+
 
 def list_quizz_files():
     """
     List all the quizzes in your library i.e json directory
     """
     click.echo(click.style("\tList of Locally Available Quizzes",
-                           fg="blue", underline = True))
-    
+                           fg="blue", underline=True))
+
     if os.listdir("json/") != []:
         for file in os.listdir("json/"):
             if file.endswith(".json"):
                 with open('json/' + file) as quiz_file:
                     try:
                         data = json.load(quiz_file)
-                        print colored("\t\t* "+splitext(file)[0], "white")
+                        print colored("\t\t* " + splitext(file)[0], "white")
                     except ValueError, e:
                         continue
             else:
@@ -77,7 +82,6 @@ def list_quizz_files():
         print("No Quizzes")
         return "No Quizz Files"
 
-    
 
 def randomize_quizes(quiz_name):
     """
@@ -107,7 +111,8 @@ def take_quiz(quiz_name):
     for quiz in quizes:
         init_screen()
         correct_ans = quiz['answer'].strip().lower()
-        ans = nonBlockingRawInput(quiz['choices'], quiz['question'], quiz['time'])
+        ans = nonBlockingRawInput(quiz['choices'], quiz[
+                                  'question'], quiz['time'])
         answer = ans.lower().strip()
         if answer in ['a', 'b', 'c', 'd']:
             if answer == correct_ans:
@@ -115,34 +120,34 @@ def take_quiz(quiz_name):
             else:
                 answers.append("x")
         elif len(answer) > 0 and answer != "timeout":
-            answers.append("i") #Invalid Choice
+            answers.append("i")  # Invalid Choice
         else:
             answers.append('timeout')
     print colored("\n\t\t****Time Out***", "red")
-    result =[]
+    result = []
     timed_out = len([x for x in answers if x == "timeout"])
-    wrong =len([x for x in answers if x == "x"])
-    invalid =len([x for x in answers if x == "i"])
-    correct = len(quizes)-(timed_out + wrong + invalid)
-    result.append(timed_out) #Timed out answers
-    result.append(wrong)     #Wrong answers
-    result.append(invalid)   #Invalid
-    result.append(correct)   #Correct  
-    total_questions = len(quizes) #Total questions
-    results = [["Timed Out Questions", result[0]], ["Wrong Score", result[1]], ["Invalid Answers",result[2]], ["Total Questions", str(total_questions)], ["Correct Score",str(result[3])+"/"+str(total_questions)]]
+    wrong = len([x for x in answers if x == "x"])
+    invalid = len([x for x in answers if x == "i"])
+    correct = len(quizes) - (timed_out + wrong + invalid)
+    result.append(timed_out)  # Timed out answers
+    result.append(wrong)  # Wrong answers
+    result.append(invalid)  # Invalid
+    result.append(correct)  # Correct
+    total_questions = len(quizes)  # Total questions
+    results = [["Timed Out Questions", result[0]], ["Wrong Score", result[1]], ["Invalid Answers", result[2]], [
+        "Total Questions", str(total_questions)], ["Correct Score", str(result[3]) + "/" + str(total_questions)]]
     print("\n")
-    click.echo(click.style(tabulate(results, tablefmt = "fancy_grid"), fg="green")) 
+    click.echo(click.style(tabulate(results, tablefmt="fancy_grid"), fg="green"))
     return type(result)
 
 
-
 def download_quiz(quiz_name):
-    result = firebase.get('/'+ quiz_name, None)
+    result = firebase.get('/' + quiz_name, None)
     if result == None:
-        print colored("No results from remote DB", "red")   
-        return False     
+        print colored("No results from remote DB", "red")
+        return False
     try:
-        with open('json/'+ quiz_name +'.json', 'w') as outfile:
+        with open('json/' + quiz_name + '.json', 'w') as outfile:
             json.dump(result, outfile)
         return True
     except:
@@ -150,23 +155,24 @@ def download_quiz(quiz_name):
         raise IOError
 
 schema = {
-    "type" : "object",
+    "type": "object",
     "properties": {
-        "question" : { "type" : "string"},
-        "choices" : {  "type" : "object",
-                        "properties": {
-                            "A" : {"type" : "string"},
-                            "B" : {"type" : "string"},
-                            "C" : {"type" : "string"},
-                            "D" : {"type" : "string"}
-                        }
+        "question": {"type": "string"},
+        "choices": {"type": "object",
+                    "properties": {
+                        "A": {"type": "string"},
+                        "B": {"type": "string"},
+                            "C": {"type": "string"},
+                            "D": {"type": "string"}
+                    }
                     },
-        "ans" : { "type" : "string" },
-        "time" : { "type" : "number" }
+        "ans": {"type": "string"},
+        "time": {"type": "number"}
     },
-    "additionalProperties" : False,
-    "required" : ['question', "choices", "ans", "time"]
+    "additionalProperties": False,
+    "required": ['question', "choices", "ans", "time"]
 }
+
 
 def validate_json(instance, schema):
     try:
@@ -174,6 +180,7 @@ def validate_json(instance, schema):
         return "Valid JSON Format"
     except Exception, e:
         return "Invalid JSON Format"
+
 
 def import_quiz(quiz_path):
     if not os.path.isfile(quiz_path):
@@ -191,7 +198,7 @@ def import_quiz(quiz_path):
                 if validation == "Invalid JSON Format":
                     return validation
             copy2(quiz_path, "json")
-            print colored("Quizz File {0} Imported".format(quiz_path), "green")  
+            print colored("Quizz File {0} Imported".format(quiz_path), "green")
         except ValueError, e:
             print colored("Invalid JSON Format", "red")
             return "Invalid JSON Format"
@@ -199,10 +206,13 @@ def import_quiz(quiz_path):
 
 def list_remote_quizzes():
     data = firebase.get("/", None)
-    click.echo(click.style('\tList of Online Questions', fg='green', underline="=", bold=True))
+    click.echo(click.style('\tList of Online Questions',
+                           fg='green', underline="=", bold=True))
     for key, val in data.iteritems():
-        click.echo(click.style("\t\t* "+key, fg="green"))
-    click.echo(click.style("\t\t<Download the querries to access them>", fg="yellow"))
+        click.echo(click.style("\t\t* " + key, fg="green"))
+    click.echo(click.style(
+        "\t\t<Download the querries to access them>", fg="yellow"))
+
 
 def upload_quiz(quiz_name):
     try:
@@ -217,5 +227,3 @@ def upload_quiz(quiz_name):
     except IOError, e:
         print colored("Error Opening File", "red")
         return False
-
-    
--- quizzapp_tests.py
+++ quizzapp_tests.py
@@ -6,118 +6,123 @@ warnings.filterwarnings("ignore")
 
 
 class TestQuizzApp(unittest.TestCase):
-	"""
-	Testing functionality of different quizzapp functions
-	"""
-	
-	def test_take_quiz(self):
-		"""
-		Test case if return type is 'list'
-		"""
-		self.assertIs(quizzactions.take_quiz("sports"), list, msg="Return type not a list")
+    """
+    Testing functionality of different quizzapp functions
+    """
 
-	def test_take_quiz2(self):
-		"""
-		Test case if quizz does not Exist. Exception is Raised
-		"""
-		with self.assertRaises(Exception):
-			quizzactions.take_quiz("sportshh")
+    def test_take_quiz(self):
+        """
+        Test case if return type is 'list'
+        """
+        self.assertIs(quizzactions.take_quiz("sports"),
+                      list, msg="Return type not a list")
 
-	def test_download_quiz(self):
-		"""
-		Test if the quizz is not on the remote repo
-		"""
-		self.assertEqual(quizzactions.download_quiz("gfgggf"), False, msg="The Quizz is Available on the Remote Repo")
+    def test_take_quiz2(self):
+        """
+        Test case if quizz does not Exist. Exception is Raised
+        """
+        with self.assertRaises(Exception):
+            quizzactions.take_quiz("sportshh")
 
-	def test_quizz_validation(self):
-		"""
-		Testing valid JSON data
-		"""
-		json_data = {
-    		"question": "Who was the second president of Kenya",
-		    "choices": {
-		        "A": "Me",
-		        "B": "You",
-		        "C": "Him",
-		        "D": "Her"
-		    },
-		    "answer": "C",
-		    "time": 5
-		}
+    def test_download_quiz(self):
+        """
+        Test if the quizz is not on the remote repo
+        """
+        self.assertEqual(quizzactions.download_quiz("gfgggf"),
+                         False, msg="The Quizz is Available on the Remote Repo")
 
+    def test_quizz_validation(self):
+        """
+        Testing valid JSON data
+        """
+        json_data = {
+            "question": "Who was the second president of Kenya",
+            "choices": {
+                "A": "Me",
+                "B": "You",
+                "C": "Him",
+                "D": "Her"
+            },
+            "answer": "C",
+            "time": 5
+        }
 
-		schema = {
-		    "type": "object",
-		    "properties": {
-		        "question": {"type": "string"},
-		        "choices": {"type": "object",
-		                    "properties": {
-		                        "A": {"type": "string"},
-		                        "B": {"type": "string"},
-		                            "C": {"type": "string"},
-		                            "D": {"type": "string"}
-		                    }
-		                    },
-		        "answer": {"type": "string"},
-		        "time": {"type": "number"}
-		    },
-		    "additionalProperties": False,
-		    "required": ['question', "choices", "answer", "time"]
-		}
-		self.assertEqual(quizzactions.validate_json(json_data, schema), "Valid JSON Format", msg="Invalid JSON format")
+        schema = {
+            "type": "object",
+            "properties": {
+                "question": {"type": "string"},
+                "choices": {"type": "object",
+                            "properties": {
+                                "A": {"type": "string"},
+                                "B": {"type": "string"},
+                                    "C": {"type": "string"},
+                                    "D": {"type": "string"}
+                            }
+                            },
+                "answer": {"type": "string"},
+                "time": {"type": "number"}
+            },
+            "additionalProperties": False,
+            "required": ['question', "choices", "answer", "time"]
+        }
+        self.assertEqual(quizzactions.validate_json(
+            json_data, schema), "Valid JSON Format", msg="Invalid JSON format")
 
+    def test_quizz_validation2(self):
+        """
+        Tests for wrong JSON format quizz
+        """
+        schema = {
+            "type": "object",
+            "properties": {
+                "question": {"type": "string"},
+                "choices": {"type": "object",
+                            "properties": {
+                                "A": {"type": "string"},
+                                "B": {"type": "string"},
+                                    "C": {"type": "string"},
+                                    "D": {"type": "string"}
+                            }
+                            },
+                "answer": {"type": "string"},
+                "time": {"type": "number"}
+            },
+            "additionalProperties": False,
+            "required": ['question', "choices", "answer", "time"]
+        }
 
-	def test_quizz_validation2(self):
-		"""
-		Tests for wrong JSON format quizz
-		"""
-		schema = {
-		    "type": "object",
-		    "properties": {
-		        "question": {"type": "string"},
-		        "choices": {"type": "object",
-		                    "properties": {
-		                        "A": {"type": "string"},
-		                        "B": {"type": "string"},
-		                            "C": {"type": "string"},
-		                            "D": {"type": "string"}
-		                    }
-		                    },
-		        "answer": {"type": "string"},
-		        "time": {"type": "number"}
-		    },
-		    "additionalProperties": False,
-		    "required": ['question', "choices", "answer", "time"]
-		}
+        wrong_data = {
+            "question": "hgjgf",
+            "choices": {
+                "A": "jjfg",
+                "B": "dhjkfd"
+            }
+        }
 
-		wrong_data = {
-		    "question": "hgjgf",
-		    "choices": {
-		        "A": "jjfg",
-		        "B": "dhjkfd"
-		    }
-		}
+        self.assertEqual(quizzactions.validate_json(
+            wrong_data, schema), "Invalid JSON Format", msg="Invalid JSON format")
 
-		self.assertEqual(quizzactions.validate_json(wrong_data, schema), "Invalid JSON Format", msg="Invalid JSON format")
+    def test_import_quiz(self):
+        """
+        Test for importing a file that does not exist
+        """
+        self.assertEqual(quizzactions.import_quiz("json.txt"),
+                         "Invalid Path", "The file Exists")
 
-	def test_import_quiz(self):
-		"""
-		Test for importing a file that does not exist
-		"""
-		self.assertEqual(quizzactions.import_quiz("json.txt"),"Invalid Path", "The file Exists")
+    def test_import_quiz2(self):
+        """
+        Test for importing a file that is not *.json
+        """
+        self.assertEqual(quizzactions.import_quiz("invalid.txt"),
+                         "Not A JSON File", "The file is a JSON File")
 
-	def test_import_quiz2(self):
-		"""
-		Test for importing a file that is not *.json
-		"""
-		self.assertEqual(quizzactions.import_quiz("invalid.txt"),"Not A JSON File", "The file is a JSON File")
-
-	def test_import_quiz3(self):
-		"""
-		Test for importing a file that is *.json but wrong format
-		"""
-		self.assertEqual(quizzactions.import_quiz("invalid.json"),"Invalid JSON Format", "The file is a valid Quizz File")
+    def test_import_quiz3(self):
+        """
+        Test for importing a file that is *.json but wrong format
+        """
+        self.assertEqual(quizzactions.import_quiz("invalid.json"),
+                         "Invalid JSON Format", "The file is a valid Quizz File")
 
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
